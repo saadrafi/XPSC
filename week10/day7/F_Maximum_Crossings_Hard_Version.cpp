e490974c776b22de31e6937c7c556736ac7f07be
saadrafi <saadrafi81@gmail.com>
2024-05-31 22:40:56 +0600

    problem solved at w10d7

---
+++ week10/day7/F_Maximum_Crossings_Hard_Version.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T>
+using pbds = tree<T, null_type, less_equal<T>, rb_tree_tag, tree_order_statistics_node_update>;
+#define ll long long
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> a(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> a[i];
+        }
+
+        pbds<ll> s;
+        ll ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans += s.size() - s.order_of_key(a[i]);
+            s.insert(a[i]);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
